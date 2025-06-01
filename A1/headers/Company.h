d07cb121dff3e6d73c0e8e5556a118c65690467e
Mebanseibor <mebanseiborlm@gmail.com>
2025-06-01 17:15:02 +0530

    Added a header file with Employee class

---
+++ A1/headers/Company.h
@@ -0,0 +1,12 @@
+#ifndef Company
+    #include <iostream>
+    
+    namespace com{
+        class Employee{
+            public:
+            int id = 0;
+            std::string name = "default name";
+            int salaryBase = 0;
+        };
+    }
+#endif
---
+++ A1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "headers/Company.h"
+
+
+using namespace com;
+int main(){
+    Employee e1 = Employee();
+    std::cout << e1.name;
+    
+    return 0;
+}
