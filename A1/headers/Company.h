52c5420053104dad795713bee0dc398a2e828073
Mebanseibor <mebanseiborlm@gmail.com>
2025-06-01 19:07:58 +0530

    Added subclass for SalesPerson, TechLead
    - Updated manager and employee subclass
    - Added constants for bonuses for each metric
    - Implemented proper deconstructors for each class

--- A1/headers/Company.h
+++ A1/headers/Company.h
@@ -1,5 +1,8 @@
 #ifndef Company
     #include <iostream>
+    const double bonusManagersPercentage = 20;
+    const double bonusDeveloperValue = 1000;
+    const double bonusSalesPersonValue = 500;
     
     namespace com{
         class Employee{
@@ -8,28 +11,121 @@
             std::string name = "default name";
             double salaryBase = 10000;
 
-            double calculateSalary(){ return salaryBase; }
+            void calculateSalary(){ }
+
+            Employee(){ }
+
+            Employee(int id, std::string name){
+                this->id = id;
+                this->name = name;
+                calculateSalary();
+            }
 
             void printDetails(){
                 std::cout << "ID:\t" << id << "\n";
                 std::cout << "Name:\t" << name << "\n";
                 std::cout << "Salary Base:\t" << salaryBase << "\n";
             }
+
+            virtual ~Employee(){ std::cout << "Base object with name " << name << " has been destroyed\n"; }
         };
 
         class Manager : Employee{
-            double percentage = 20;
             double salary = 0;
 
-            void calculateSalary(){ salary = salaryBase * (1 + percentage/100); }
+            void calculateSalary(){ salary = salaryBase * (1 + bonusManagersPercentage/100); }
 
             public:
-            Manager(){ calculateSalary(); }
+            Manager(int id, std::string name){
+                this->id = id;
+                this->name = name;
+                calculateSalary();
+            }
 
             void printDetails(){
                 Employee::printDetails();
+                std::cout << "Bonus Percentage over base salary:\t" << bonusManagersPercentage << "\n";
                 std::cout << "Salary:\t" << salary << "\n";
             }
+
+            ~Manager(){ std::cout << "Destroying Manager:\t" << name << "\n"; }
+        };
+
+        class Developer : Employee{
+            int countProjects = 0;
+            double salary = 0;
+            
+            void calculateSalary(){ salary = salaryBase + (bonusDeveloperValue*countProjects); }
+
+            public:
+            Developer(int id, std::string name, int projects){
+                this->id = id;
+                this->name = name;
+                countProjects = projects;
+                calculateSalary();
+            }
+
+            void printDetails(){
+                Employee::printDetails();
+                std::cout << "Projects Done:\t" << countProjects << "\n";
+                std::cout << "Bonus per project:\t" << bonusDeveloperValue << "\n";
+                std::cout << "Salary:\t" << salary << "\n";
+            }
+
+            ~Developer(){ std::cout << "Destroying Developer:\t" << name << "\n"; }
+        };
+
+        class SalesPerson : Employee{
+            int monthlySales = 0;
+            double salary = 0;
+            
+            void calculateSalary(){ salary = salaryBase + (bonusSalesPersonValue*monthlySales); }
+
+            public:
+            SalesPerson(int id, std::string name, int monthlySales){
+                this->id = id;
+                this->name = name;
+                this->monthlySales = monthlySales;
+                calculateSalary();
+            }
+
+            void printDetails(){
+                Employee::printDetails();
+                std::cout << "Monthly Sales:\t" << monthlySales<< "\n";
+                std::cout << "Bonus per Monthly Sales:\t" << bonusSalesPersonValue << "\n";
+                std::cout << "Salary:\t" << salary << "\n";
+            }
+
+            ~SalesPerson(){ std::cout << "Destroying Sales Person:\t" << name << "\n"; }
+        };
+
+        class TechLead : Employee{
+            int countProjects = 0;
+            int salary = 0;
+
+            void calculateSalary(){
+                double bonusManager = salaryBase * (bonusManagersPercentage/100); 
+                double bonusDeveloper = bonusDeveloperValue*countProjects; 
+                salary = salaryBase + bonusManager + bonusDeveloper;
+            }
+
+            public:
+            TechLead(int id, std::string name, int projects){
+                this->id = id;
+                this->name = name;
+                countProjects = projects;
+                calculateSalary();
+            }
+
+            void printDetails(){
+                Employee::printDetails();
+                std::cout << "Projects Done:\t" << countProjects << "\n";
+                std::cout << "Bonus per project:\t" << bonusDeveloperValue << "\n";
+                std::cout << "Bonus Percentage over base salary:\t" << bonusManagersPercentage << "\n";
+                std::cout << "Salary:\t" << salary << "\n";
+            }
+
+            ~TechLead(){ std::cout << "Destroying Tech Lead:\t" << name << "\n"; }
         };
     }
 #endif
--- A1/main.cpp
+++ A1/main.cpp
@@ -4,14 +4,30 @@
 
 using namespace com;
 int main(){
+    std::cout << "\t--- Start of Program ---\n\n\n";
+
     std::cout << "Employee:\n";
-    Employee employee = Employee();
+    Employee employee = Employee(0, "Emily");
     employee.printDetails();
 
 
     std::cout << "\nManager:\n";
-    Manager manager = Manager();
+    Manager manager = Manager(1, "Medon");
     manager.printDetails();
     
+    std::cout << "\nDeveloper:\n";
+    Developer developer = Developer(2, "Daphi", 3);
+    developer.printDetails();
+    
+    std::cout << "\nSales Person:\n";
+    SalesPerson salesPerson = SalesPerson(3, "Simon", 2);
+    salesPerson.printDetails();
+    
+    std::cout << "\nTech Lead:\n";
+    TechLead techLead = TechLead(4, "Timothy", 5);
+    techLead.printDetails();
+
+    std::cout << "\n\n\t--- End of Program ---\n\n\n";
+
     return 0;
 }
