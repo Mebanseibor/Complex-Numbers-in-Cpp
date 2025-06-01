b04480798a93f990d2dfdc0c2f4113524444edfc
Mebanseibor <mebanseiborlm@gmail.com>
2025-06-01 16:56:42 +0530

    Implemented overloaded operators << and >>
    - Removed helper functions
    - Removed display function
    - Removed input function
    - Removed displayInputs function

--- A2/main.cpp
+++ A2/main.cpp
@@ -44,20 +44,15 @@ class Complex{
         return result;
     }
 
-    // helper functions
-    void display(){
-        char symbol = '\0';
-        if (imag>=0) symbol = '+';
-        std::cout << real << symbol << imag << "i\n";
-    }
-
     double getReal() const {return real;}
     double getImag() const {return imag;}
     double& getReal() {return real;}
     double& getImag() {return imag;}
-    void setReal(double real){this->real = real;}
-    void setImag(double imag){this->imag = imag;}
 };
+
+
+
+
 
 std::ostream& operator << (std::ostream& os, const Complex& c) {
     os << c.getReal();
@@ -73,38 +68,27 @@ std::istream& operator >> (std::istream& is, Complex& c) {
     return is;
 }
 
-Complex input(){
-    double r, i;
-    std::cout << "Enter the real part: ";
-    std::cin >> r;
-    std::cout << "Enter the imaginary part: ";
-    std::cin >> i;
-    return Complex(r, i);
-}
 
-void displayInputs(Complex c1, Complex c2){
-    std::cout << "Complex Number 1:\n";
-    c1.display();
 
-    std::cout << "\nComplex Number 2:\n";
-    c2.display();
-}
+
+
 
 int main(){
     std::cout << "--- Start of program ---\n";
 
-    // take inputs
+    // demonstrating >> operator overloading
     std::cout << "Input 2 complex numbers\n";
     
+    Complex c1 = Complex();
     std::cout << "Enter the first complex number:\n";
-    Complex c1 = input();
+    std::cin >> c1;
     std::cout << "\n";
 
+    Complex c2 = Complex();
     std::cout << "Enter the second complex number:\n";
-    Complex c2 = input();
+    std::cin >> c2;
     std::cout << "\n";
 
-    displayInputs(c1, c2);
 
 
 
@@ -115,14 +99,11 @@ int main(){
 
     std::cout << "\n\nResults of their Sum, Difference and Products\n";
 
-    std::cout << "Sum:\n";
-    cSum.display();
+    std::cout << "Sum:\n" << cSum << "\n";
 
-    std::cout << "\nDiff:\n";
-    cDiff.display();
+    std::cout << "\nDiff:\n" << cDiff << "\n";
 
-    std::cout << "\nProduct:\n";
-    cProduct.display();
+    std::cout << "\nProduct:\n" << cProduct << "\n";
 
 
 
@@ -143,7 +124,7 @@ int main(){
 
 
 
-    // demonstrating << operator overloadding
+    // demonstrating << operator overloading
     std::cout << "\n\nResults:\n";
     std::cout << "First complex number:\t" << c1 << "\n";
     std::cout << "Second complex number:\t" << c2 << "\n";
